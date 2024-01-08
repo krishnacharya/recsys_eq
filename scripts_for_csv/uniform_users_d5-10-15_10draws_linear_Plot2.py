847abc35360cb9059353465c91f178de684ab8cb
KVA <krishnacharya97@gmail.com>
2024-01-07 19:33:48 -0500

    adds num_iters within each d, draw

--- scripts_for_csv/uniform_users_d5-10-15_10draws_linear_Plot2.py
+++ scripts_for_csv/uniform_users_d5-10-15_10draws_linear_Plot2.py
@@ -21,10 +21,11 @@ with tqdm(total = len(dimensions) * user_draws * Br_runs) as pbar:
         for draw in range(user_draws):
             ue = generate_uniform_users(dimension = d, num_users = num_users)
             PEng = ProducersEngagementGame(num_producers = num_prod, users = Users(ue), prob = 'linear')
+            num_iter = [] # will be an array of length Br_runs
             for _ in range(Br_runs): # run best response dynamics each time
-                PEng.best_response_dynamics(verbose=False)
+                NE, iters = PEng.best_response_dynamics(verbose=False)
+                num_iter.append(iters)
                 pbar.update(1)
-
             if len(PEng.BR_dyna_NE) > 1: # two or more NE save that PEng
                 print("Found more than 1 NE for the Producer engagement game")
                 with open(f'morethan1NE_dimension_{d}_draw_{draw}.pickle', 'wb') as handle:
@@ -34,12 +35,12 @@ with tqdm(total = len(dimensions) * user_draws * Br_runs) as pbar:
                 print("Found no NE for the Producer engagement game")
                 with open(f'noNE_dimension_{d}_draw_{draw}.pickle', 'wb') as handle:
                     pickle.dump(PEng, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
             NE = np.array(PEng.BR_dyna_NE.pop())
             di = {"dimension": d,
                 "user_draw": draw, 
                 "num_users": num_users,
                 "num_producers": num_prod,
+                "num_iters": np.array(num_iter),
                 "NE": NE, # pop the one tuple in this BR_dyna_NE set
                 "producer_dist": NE / num_prod, # fraction of producers on each dimension
                 "user_dist": np.sum(ue, axis = 0) / num_users # fraction of users on each dimension
