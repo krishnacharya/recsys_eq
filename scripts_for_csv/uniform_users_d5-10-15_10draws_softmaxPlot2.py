9066b0753fb6943e7788a2c6ceea3b394a1a05ad
KVA <krishnacharya97@gmail.com>
2024-01-07 19:19:53 -0500

    adds experiment for 3 dimension, uniform users, linear and softmax

---
+++ scripts_for_csv/uniform_users_d5-10-15_10draws_softmaxPlot2.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append('../source/')
+import numpy as np
+from Users import Users
+from Producers import ProducersEngagementGame
+from embeddings import generate_uniform_users
+import pickle
+import pandas as pd
+from tqdm import tqdm
+
+num_users = 10000 # fixed
+num_prod = 100 # fixed
+dimensions = [5, 10, 15] # vary across 3 dims and see the kind of specialization seen at NE
+
+user_draws = 10 # number of draws of uniform user embeddings for each num_user x dimension
+Br_runs = 20
+
+res = []
+with tqdm(total = len(dimensions) * user_draws * Br_runs) as pbar:
+    for d in dimensions:
+        for draw in range(user_draws):
+            ue = generate_uniform_users(dimension = d, num_users = num_users)
+            PEng = ProducersEngagementGame(num_producers = num_prod, users = Users(ue), prob = 'softmax')
+            for _ in range(Br_runs): # run best response dynamics each time
+                PEng.best_response_dynamics(verbose=False)
+                pbar.update(1)
+
+            if len(PEng.BR_dyna_NE) > 1: # two or more NE save that PEng
+                print("Found more than 1 NE for the Producer engagement game")
+                with open(f'morethan1NE_dimension_{d}_draw_{draw}.pickle', 'wb') as handle:
+                    pickle.dump(PEng, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+            if len(PEng.BR_dyna_NE) == 0: # no NE found, save that PEng
+                print("Found no NE for the Producer engagement game")
+                with open(f'noNE_dimension_{d}_draw_{draw}.pickle', 'wb') as handle:
+                    pickle.dump(PEng, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+            NE = np.array(PEng.BR_dyna_NE.pop())
+            di = {"dimension": d,
+                "user_draw": draw, 
+                "num_users": num_users,
+                "num_producers": num_prod,
+                "NE": NE, # pop the one tuple in this BR_dyna_NE set
+                "producer_dist": NE / num_prod, # fraction of producers on each dimension
+                "user_dist": np.sum(ue, axis = 0) / num_users # fraction of users on each dimension
+            }
+            res.append(di)
+df = pd.DataFrame(res)
+df.to_pickle('../saved_frames/uniform_users_softmax_d5-10-15_10draws.pkl')
+        
+        
--- source/embeddings.py
+++ source/embeddings.py
@@ -5,14 +5,39 @@ from surprise.model_selection import cross_validate
 from sklearn.preprocessing import normalize
 import numpy as np
 
-def generate_uniform_user(dimension):
-  #unifrom sampling form the probability simplex
-  #reference : https://cs.stackexchange.com/questions/3227/uniform-sampling-from-a-simplex
-  return np.diff([0] + sorted(np.random.uniform(size=dimension-1)) + [1])
+def generate_uniform_users(dimension, num_users = 10000) -> np.array:
+  '''
+    Get a numpy array of shape num_users x dimension
+    with each row representing a user on the proability simplex, i.e.\sum_j x_ij = 1
+  '''
+  def generate_uniform_user(dimension):
+    #unifrom sampling form the probability simplex
+    #reference : https://cs.stackexchange.com/questions/3227/uniform-sampling-from-a-simplex
+    return np.diff([0] + sorted(np.random.uniform(size=dimension-1)) + [1])
+  return np.array([generate_uniform_user(dimension=dimension) for _ in range(num_users)])
 
-def get_user_embeddings(user_dimension, data: surprise.Dataset):
+def get_user_embeddings_movielens100k(user_dimension):
+  '''
+    Gets embeddings for movielens 100k dataset
+    Returns
+    User embeddings, and L1 normalized user embeddings
+  '''
+  data = Dataset.load_builtin("ml-100k")
   algo = NMF(n_factors=user_dimension)
-  # algo = NMF(n_factors=user_dimension, init_low=0, init_high=10)
   cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=False)
-  users = normalize(algo.pu, "l1")
-  return users
+  normalized_embeddings = normalize(algo.pu, "l1")
+  return algo.pu, normalized_embeddings
+
+def user_embeddings_movielens100k_nmf(dimension):
+  '''
+    Uses parameters from Krauth
+    Returns
+    User embeddings, and L1 normalized user embeddings
+  '''
+  algo = NMF(n_factors=dimension, biased =False, reg_pu=0.08, reg_qi=0.08, n_epochs = 128)
+  data =  Dataset.load_builtin("ml-100k")
+  full_data = data.build_full_trainset()
+  algo.fit(full_data)
+  normalized_embeddings = normalize(algo.pu, "l1")
+  return algo.pu, normalized_embeddings
+  
