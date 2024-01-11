eb2281d44ad48969e57edab65532575760718be0
KVA <krishnacharya97@gmail.com>
2024-01-10 23:57:39 -0500

    adds skewed synthetic

--- source/embeddings.py
+++ source/embeddings.py
@@ -5,9 +5,10 @@ from surprise.model_selection import cross_validate
 from sklearn.preprocessing import normalize
 import numpy as np
 
-def generate_uniform_user(dimension):
+def generate_uniform_user(dimension) -> np.array:
     '''
       Sample a single user from the probability simplex of dimension=dimension \sum_j x_j = 1
+      shape (dim, )
     '''
     #unifrom sampling form the probability simplex
     #reference : https://cs.stackexchange.com/questions/3227/uniform-sampling-from-a-simplex
@@ -20,6 +21,15 @@ def generate_uniform_users(dimension, num_users = 10000) -> np.array:
   '''
   return np.array([generate_uniform_user(dimension=dimension) for _ in range(num_users)])
 
+def generate_skewed_users(dimension, num_users = 10000) -> np.array:
+  '''
+    Get a numpy array of shape num_users x dimension
+    with each row representing a user on the proability simplex, i.e.\sum_j x_ij = 1
+  '''
+  weights = np.sort(generate_uniform_user(dimension)) # shape d, from the probbability simplex
+  ue = generate_uniform_users(dimension, num_users = num_users) # Num users, d
+  return normalize(ue * weights, norm = "l1")
+ 
 def generate_sorted_synthetic(dimension, num_users = 10000) -> np.array:
   ue = generate_uniform_users(dimension, num_users = num_users)
   ue.sort(axis=1) # sorts each row ascending
@@ -34,7 +44,7 @@ def get_user_embeddings_movielens100k(user_dimension):
   data = Dataset.load_builtin("ml-100k")
   algo = NMF(n_factors=user_dimension)
   cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=False)
-  normalized_embeddings = normalize(algo.pu, "l1")
+  normalized_embeddings = normalize(algo.pu,  norm = "l1")
   return algo.pu, normalized_embeddings
 
 def user_embeddings_movielens100k_nmf(dimension):
