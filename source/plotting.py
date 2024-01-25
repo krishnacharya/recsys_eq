bca7f93082cf646c629985e7a1b1dcf86ac310ca
KVA <krishnacharya97@gmail.com>
2024-01-25 16:08:48 -0500

    organizes files for figures

--- source/plotting.py
+++ source/plotting.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def user_producer_2bar_plot(pd, ud, filedestname:str): #producer distribution and user distribution
     #TODO add savefig too
@@ -44,3 +45,17 @@ def user_producer_2bar_plot_display(pd, ud): #producer distribution and user dis
 
     plt.xticks(range(dim))
     plt.show()
+
+def plot_and_save(dims, df_linear:pd.DataFrame, df_softmax:pd.DataFrame, name:str, nprod = 100, seed=17): # representative plots for ud and pd
+    distributions = ['user_dist', 'producer_dist']
+    for d in dims:
+        df_linear_temp = df_linear[(df_linear['seed'] ==  seed) & (df_linear['dimension'] ==  d) & \
+                         (df_linear['nprod'] == nprod)][distributions]
+        df_softmax_temp = df_softmax[(df_softmax['seed'] ==  seed) & (df_softmax['dimension'] ==  d)\
+                           & (df_softmax['nprod'] == nprod)][distributions]
+
+        ud_linear, pd_linear = df_linear_temp['user_dist'].item(), df_linear_temp['producer_dist'].item()
+        ud_softmax, pd_softmax = df_softmax_temp['user_dist'].item(), df_softmax_temp['producer_dist'].item()
+
+        user_producer_2bar_plot(pd_linear, ud_linear, f'../plots/{name}_linear_dim{d}_{nprod}prod.pdf')
+        user_producer_2bar_plot(pd_softmax, ud_softmax, f'../plots/{name}_softmax_dim{d}_{nprod}prod.pdf')
