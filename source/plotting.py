5ba4ddb7895874c029a3e134676cce7f368a21ab
KVA <krishnacharya97@gmail.com>
2024-01-25 16:55:02 -0500

    reorg2

--- source/plotting.py
+++ source/plotting.py
@@ -46,6 +46,41 @@ def user_producer_2bar_plot_display(pd, ud): #producer distribution and user dis
     plt.xticks(range(dim))
     plt.show()
 
+def plot_4dim_numiter_errbar(df_agg, filename): # best response mean number of iterations to NE and standard deviation
+  dims = df_agg['dimension'].unique()
+  nprods = df_agg['num_prod'].unique()
+  print(dims, nprods)
+  
+
+  data_dim5 = {
+    'x': nprods,
+    'y': df_agg[df_agg['dimension'] == 5]['mean_iter_to_NE'],
+    'yerr': df_agg[df_agg['dimension'] == 5]['std_iters_to_NE']}
+  
+  data_dim10 = {
+    'x': nprods+0.3,   # adding offsets to x to make standard error bars non-overlapping
+    'y': df_agg[df_agg['dimension'] == 10]['mean_iter_to_NE'],
+    'yerr': df_agg[df_agg['dimension'] == 10]['std_iters_to_NE']}
+
+  data_dim15 = {
+    'x': nprods-0.3,
+    'y': df_agg[df_agg['dimension'] == 15]['mean_iter_to_NE'],
+    'yerr': df_agg[df_agg['dimension'] == 15]['std_iters_to_NE']}
+  
+  data_dim20 = {
+    'x': nprods+0.5,
+    'y': df_agg[df_agg['dimension'] == 20]['mean_iter_to_NE'],
+    'yerr': df_agg[df_agg['dimension'] == 20]['std_iters_to_NE']}
+  line_styles = [':o',':s' ,':o' , ':s'] # dotted circle and square
+  plt.figure()
+  for idx, data in enumerate([data_dim5, data_dim10, data_dim15, data_dim20]):
+      plt.errorbar(**data, fmt=line_styles[idx], capsize=3, capthick=1, elinewidth=1, \
+                   alpha=0.9, markersize=4, label = f'dimension = {dims[idx]}')
+  plt.legend(loc= "upper left")
+  plt.xlabel("Number of producers")
+  plt.ylabel("Iterations to NE")
+  plt.savefig(filename, bbox_inches='tight')
+
 def plot_and_save(dims, df_linear:pd.DataFrame, df_softmax:pd.DataFrame, name:str, nprod = 100, seed=17): # representative plots for ud and pd
     distributions = ['user_dist', 'producer_dist']
     for d in dims:
@@ -57,5 +92,5 @@ def plot_and_save(dims, df_linear:pd.DataFrame, df_softmax:pd.DataFrame, name:st
         ud_linear, pd_linear = df_linear_temp['user_dist'].item(), df_linear_temp['producer_dist'].item()
         ud_softmax, pd_softmax = df_softmax_temp['user_dist'].item(), df_softmax_temp['producer_dist'].item()
 
-        user_producer_2bar_plot(pd_linear, ud_linear, f'../plots/{name}_linear_dim{d}_{nprod}prod.pdf')
-        user_producer_2bar_plot(pd_softmax, ud_softmax, f'../plots/{name}_softmax_dim{d}_{nprod}prod.pdf')
+        user_producer_2bar_plot(pd_linear, ud_linear, f'../plots/udpd/{name}_linear_dim{d}_{nprod}prod.pdf')
+        user_producer_2bar_plot(pd_softmax, ud_softmax, f'../plots/udpd/{name}_softmax_dim{d}_{nprod}prod.pdf')
