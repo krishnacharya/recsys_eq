bf7b5e5f334786b5bcc35b88d65898fc2ae0639f
KVA <krishnacharya97@gmail.com>
2024-03-24 18:57:19 -0400

    adds temperature to softmax_probability, get_all_engagement_utilities, and common function protoype for self.probability_function variable

--- source/Producers.py
+++ source/Producers.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def random_rec_utilities(num_prod:int, user_array:np.ndarray) -> tuple[float, int, np.ndarray]:
     '''
-        p_i(c_k) = 1/num_prod, probability of user coming any producer is uniform
+        p_i(c_k) = 1/num_prod, probability of user going to producer is uniform
         thus the utility for producer i is 1/num_prod * (\sum_k=1^num users <s_i, c_k>)
         it is best for each prodcuer to play cordinate maximizer of \sum_k=1^numuser c_k
 
@@ -20,27 +20,28 @@ def random_rec_utilities(num_prod:int, user_array:np.ndarray) -> tuple[float, in
     max_cord_sum = ua_sum[best_strat_index]
     return max_cord_sum / num_prod, best_strat_index, user_array[:, best_strat_index]
 
-def linear_probability(content_vector:np.ndarray, remaining_array:np.ndarray, user_array:np.ndarray)->np.ndarray:
-    '''
+def linear_probability(content_vector:np.ndarray, remaining_array:np.ndarray, user_array:np.ndarray, temp = 1)->np.ndarray: # hacky fix for now, adding temp here which is to make function arguments similar to softmax_probability
+    ''' 
         content_vector : shape is (dimension,)
         remaining_array: shape in (N_producers - 1, dimension)
         user_array: shape is (N_users, dimension)
         Returns
-            numpy array of shape (N_user,), linear proabibility of each user getting recommended to content_vector    
+            numpy array of shape (N_user,), linear proabibility of each user getting recommended to `content_vector` (the producer who sets their vector)  
     '''
     product = user_array @ np.vstack((remaining_array, content_vector)).T # has shape N_user x N_prod, product_ij stores what user i rates producer j
     prob =  product / product.sum(axis=1)[:,None] # the [:, None] just reshapes the product.sum to (N_users, 1) for broadcast division
     return prob[:, -1] #prob_ij contains with what probability user i is recommended movie j; last column will have all probabilities of users going to content_vector
 
-def softmax_probability(content_vector:np.ndarray, remaining_array:np.ndarray, user_array:np.ndarray)->np.ndarray:
+def softmax_probability(content_vector:np.ndarray, remaining_array:np.ndarray, user_array:np.ndarray, temp = 1)->np.ndarray:
     '''
         content_vector : shape is (dimension,)
         remaining_array: shape in (N_producers - 1, dimension)
         user_array: shape is (N_users, dimension)
+        temp: temperature for softmax, default = 1
         Returns
             numpy array of shape (N_user,), softmax proabibility of each user getting recommended to content_vector   
     '''
-    product = np.exp(user_array @ np.vstack((remaining_array, content_vector)).T) # has shape N_user x N_prod
+    product = np.exp((user_array @ np.vstack((remaining_array, content_vector)).T) / temp) # has shape N_user x N_prod, # TODO temperature added
     prob = product / product.sum(axis=1)[:, None] # the [:, None] just reshapes the product.sum to (N_users, 1) for broadcast division
     return prob[:, -1]
 
@@ -49,9 +50,11 @@ def engagement_utility(content_vector:np.ndarray, probs:np.ndarray, user_array:n
         Computes engagement utility for the content_vector
         Parameters:
             content_vector : shape is (dimension,)
-            probs: shape is (N_users,) i^th entry denotes the proability that user i will be recommended content_vector,
+            probs: shape is (N_users,) i^th entry denotes the probability that user i goes to this producer (which has `content vector` embedding)
                 this must be precomputed - it could be linear or softmax probability
             user_array: shape is (N_users, dimension)
+    
+    This function already assumed the probs are precomputed (could be linear, softmax ...)
     '''
     prods = user_array @ content_vector # what each user rates the content_vector shape (N_user,)
     return np.sum(probs * prods)
@@ -60,16 +63,19 @@ def producer_exposure_utility_linearserving(content_vector:np.ndarray, remaining
     '''
         Get the exposure utility for content_vector producer assuming rest producers are frozen to remaning array
         Used for exposure game class
+
+        Skip for now, is for exposure
     '''
     product = user_array @ np.vstack((remaining_array, content_vector)).T # has shape N_user x N_prod, product_ij stores what user i rates producer j
     prob =  product / product.sum(axis=1)[:,None] # the [:, None] just reshapes the product.sum to (N_users, 1) for broadcast division
     return np.sum(prob[:, -1]) # sum across all users of exposure for producer with content vector
 
-def get_all_engagement_utilities(producers:np.ndarray, user_array:np.ndarray, prob_type='linear'):
+def get_all_engagement_utilities(producers:np.ndarray, user_array:np.ndarray, prob_type='linear', temp = 1):
     '''
         producers: shape N_producers x dimension
         user_array: shape N_users x dimension
             Note: producers only contains basis vectors e.g [[(0,1,0..d wide), ..(1,0... d wide)... N_producers]]
+        temp: temperature for softmax probability
         Returns 
             dir_producers direction of basis vector for each producer, shape (N_producers, )
             engagement utility for each producer, shape (N_producers, )
@@ -80,9 +86,9 @@ def get_all_engagement_utilities(producers:np.ndarray, user_array:np.ndarray, pr
     ratings = user_array @ prodt # shape (N_user, N_prod) ratings ij has what user i rates producer j's content <c_i, s_j>
     prob = None
     if prob_type == 'linear':
-        prob = ratings / ratings.sum(axis=1)[:, None] # prob_ij stores <c_i, s_j> / sum_k <c_i, s_k>
+        prob = ratings / ratings.sum(axis=1)[:, None] # prob_ij stores <c_i, s_j> / sum_k <c_i, s_k> 
     elif prob_type == 'softmax':
-        exp_ratings = np.exp(ratings)
+        exp_ratings = np.exp(ratings / temp) # TODO temperature added
         prob = exp_ratings / exp_ratings.sum(axis=1)[:, None] # prob_ij stores exp(<c_i, s_j>) / sum_k exp(<c_i, s_k>), prob that user i goes to producer j
     else:
         raise NotImplementedError
@@ -94,16 +100,19 @@ class ProducersEngagementGame:
         Each producer's strategy space is the ball of L1 norm <= 1, restricted to positive orthant
         Goal of each producer is to maximize its engagament
     '''
-    def __init__(self, num_producers:int, users:Users, prob = 'linear'):
+    def __init__(self, num_producers:int, users:Users, prob = 'linear', temp = 1): # TODO added temperature
         self.num_producers = num_producers
         self.dimension = users.dimension
         self.users = users
         self.BR_dyna_NE = set() # Nash equilibria arising from best response dynamics, stores tuples with (n_1...n_d) # of producers in each direction
         self.BruteForce_NE = set() # Nash equilibria arising from brute force vertex search, stores tuples with (n_1...n_d) # of producers in each direction
+        self.temp = temp #temperature
         if prob == 'linear':
             self.probability_function = linear_probability
         elif prob == 'softmax':
             self.probability_function = softmax_probability
+        else:
+            raise NotImplementedError
     
     def get_best_response(self, current_vec: np.ndarray, remaining_array:np.ndarray) -> np.ndarray:
         '''
@@ -117,10 +126,10 @@ class ProducersEngagementGame:
                 due to properties of engagement utility
         '''
         # max_util = -1
-        max_util = engagement_utility(current_vec, self.probability_function(current_vec, remaining_array, self.users.user_array),  self.users.user_array)
+        max_util = engagement_utility(current_vec, self.probability_function(current_vec, remaining_array, self.users.user_array, temp = self.temp),  self.users.user_array)
         best_row = current_vec # setting best_row and max utility as what the current vector gives
         for row in np.eye(self.dimension):
-            probs = self.probability_function(row, remaining_array, self.users.user_array)
+            probs = self.probability_function(row, remaining_array, self.users.user_array, temp=self.temp)
             util = engagement_utility(row, probs, self.users.user_array)
             if util > max_util:
                 best_row = row
