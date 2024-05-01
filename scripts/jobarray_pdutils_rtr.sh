69a5b5fefd356b8ed22927fd9386c8b39fd77206
Krishna <krishnacharya97@gmail.com>
2024-05-01 02:53:44 -0400

    adds the runs across emb seeds for all the real datasets

--- scripts/jobarray_pdutils_rtr.sh
+++ scripts/jobarray_pdutils_rtr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -J jobarray_demo
-#SBATCH --array=1-3
+#SBATCH -J rtr-pdutils-5seeds-smandlinear
+#SBATCH --array=1-30
 #SBATCH -A gts-jziani3
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=24:00:00
 #SBATCH --output=./Sbatch-reports/Array_test.%A\_%a.out
 #SBATCH --error=./Sbatch-reports/Array_test.%A\_%a.error
@@ -11,12 +11,13 @@ module load anaconda3/2022.05.0.1
 conda activate recsys
 
 n=$SLURM_ARRAY_TASK_ID
-iteration=`sed -n "${n} p" demo_temp10_toy.csv`      # Get n-th line (1-indexed) of the file
+iteration=`sed -n "${n} p" demo_rtr_allseeds_smandlin.csv`      # Get n-th line (1-indexed) of the file
 echo "parameters for iteration: ${iteration}"
 
 data=$(echo ${iteration} | cut -d "," -f 1)
 prob=$(echo ${iteration} | cut -d "," -f 2)
 temp=$(echo ${iteration} | cut -d "," -f 3)
-cc=$(echo ${iteration} | cut -d "," -f 4)
+embseed=$(echo ${iteration} | cut -d "," -f 4)
+cc="config_less_seeds"
 
-python main_run.py --data ${data} --prob ${prob} --temp ${temp} --common_config ${cc}
+python main_run_pdutils_seedwise.py --data ${data} --prob ${prob} --temp ${temp} --common_config ${cc} --emb_seed ${embseed}
