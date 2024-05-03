babb10faff51730eaecf6692a2c91f2a3b88fb64
Krishna <krishnacharya97@gmail.com>
2024-05-03 02:13:36 -0400

    numiters for synth unif and ske, sm1 and linear

---
+++ scripts/jobarr_synthuniform_numiters_linear.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J synthuni-numiter-linear
+#SBATCH --array=1-40
+#SBATCH -A gts-jziani3
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=24:00:00
+#SBATCH --output=./Sbatch-reports/synthuni-numiter-linear.%A\_%a.out
+#SBATCH --error=./Sbatch-reports/synthuni-numiter-linear.%A\_%a.error
+
+module load anaconda3/2022.05.0.1
+conda activate recsys
+
+n=$SLURM_ARRAY_TASK_ID
+echo "parameters for iteration: ${iteration}"
+
+data="synth-uniform"
+prob="linear"
+temp="1"
+cc="config_less_seeds"
+
+python main_run_numiter.py --data ${data} --prob ${prob} --temp ${temp} --common_config ${cc} --runnum ${n}
